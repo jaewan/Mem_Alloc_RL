ecd19103aba3b526c84cbf04a1f7bef67854ab06
Joohwan-Seo <91165431+Joohwan-Seo@users.noreply.github.com>
2021-12-12 15:47:46 -0800

    Add files via upload

---
+++ action_space.py
@@ -0,0 +1,18 @@
+class Range():
+    def __init__(self):
+    def items():
+        return 0
+    def values():
+        return 0
+
+class Action_Space(Object):
+    def __init__(self):
+       self._range = Range()
+
+    def head_names(self):
+        return 0
+    def name2index(self, action, option)
+        return 0
+
+    def _num_head(self):
+        return 0
