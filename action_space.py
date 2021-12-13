abfe0e789c3001ad43ca92c3de7abbe27f1c4209
Jaewan Hong <dominico1004@hanmail.net>
2021-12-13 01:07:40 +0000

    dictionary in action_space

--- action_space.py
+++ action_space.py
@@ -1,18 +1,20 @@
-class Range():
-    def __init__(self):
-    def items():
-        return 0
-    def values():
-        return 0
+import csv
 
 class Action_Space(Object):
-    def __init__(self):
-       self._range = Range()
+    def __init__(self, csv_file):
+       self.csv_file = csv_file
+       self._range = {}
+       with open(csv_file) as f:
+           for row in csv.DictReader(f):
+               self._range[row['op_name']] = int(row['ofm_allocations']) + int(row['weights_allocations'])
+       self.head_names = self._range.keys()
 
     def head_names(self):
-        return 0
-    def name2index(self, action, option)
-        return 0
+	#return list of nodes
+        return self.head_names
 
     def _num_head(self):
+        return len(self.head_names)
+
+    def name2index(self, action, option)
         return 0
